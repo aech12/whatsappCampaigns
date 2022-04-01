6ed519668d15754677efa24da6bc86ae9a836c23
Alex Howez <aech-12@hotmail.com>
2022-03-31 23:32:21 -0300

    last commit

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,20 @@
+import { Link } from "remix";
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      App
+    <div className="p-5">
+      <p className="font-bold underline">Whatsapp Campaigns App</p>
+      <p className="p-5 my-5 bg-red-400 text-white text-lg">
+        This app is unfinished. The client did not contact me anymore.
+      </p>
+      <p>
+        Login and backend functionality will continue to work for some time.
+      </p>
+      <p className="">Contact me at aech-12@hotmail.</p>
+      <Link to="/login">
+        <button className="px-4 py-2 mt-2 border-black border-2 rounded">
+          Login
+        </button>
+      </Link>
     </div>
   );
 }
